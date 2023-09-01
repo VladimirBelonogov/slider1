420cad73691b6a50e5a73e8c4c3a7ac5edfff5f4
Владимир Белоногов <108150342+VladimirBelonogov@users.noreply.github.com>
2023-09-01 23:27:28 +0700

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,60 +1,66 @@
-
-
 function spoller() {
-    let currentPositionWidth = 0;
+    let currentPositionslider = 0;
     let spoller = document.querySelector('.slider__items');
     let item = document.querySelector('.slider__item');
     let items = document.querySelectorAll('.slider__item');
-    let previosItem = document.querySelector('.slider__body_next-item');
-    let nextItem = document.querySelector('.slider__body_previos-item');
-    let stepWidth = Number((item.getBoundingClientRect().width).toFixed(3));
+    let scrollLeft = document.querySelector('.slider__body_scrollLeft');
+    let scrollRight = document.querySelector('.slider__body_scrollRight');
+    let widthStep = Number((item.getBoundingClientRect().width).toFixed(3));
+    let steps = 0;
 
     function nextSlide() {
-        currentPositionWidth = currentPositionWidth - stepWidth;
-        spoller.style.right = currentPositionWidth + 'px';
-        jumpItemBack()
+        currentPositionslider = currentPositionslider - widthStep;
+        spoller.style.right = currentPositionslider + 'px';
+        jumpItemBack();
     }
     function previosSlide() {
-        currentPositionWidth = currentPositionWidth + stepWidth;
-        spoller.style.right = currentPositionWidth + 'px';
+        currentPositionslider = currentPositionslider + widthStep;
+        spoller.style.right = currentPositionslider + 'px';
     }
 
     function jumpItemBack() {
-        let positionSlider = (Number((spoller.style.right).slice(0,-2))).toFixed(3);
-        let currentElementPosition = Math.round(Number((Number(positionSlider) / stepWidth) % items.length));
-        let currentRatio = Math.abs(Math.round(Math.floor(Number(positionSlider)/Number(stepWidth)/items.length)));
-        console.log([items.length + currentElementPosition]);
-        console.log(currentElementPosition);
-        console.log(currentRatio);
-        
-        if (currentElementPosition  == 0){
-            currentElementPosition = currentElementPosition - items.length;
-        }
-        console.log(currentElementPosition);
+        let stepSlaider = Number(widthStep * (items.length));
+        let stepConvert = steps % items.length;
 
-        
-        
-        items[items.length + currentElementPosition].style.right = (Number(stepWidth * (items.length)) * currentRatio).toFixed(3) + 'px';
+        if (steps < 0) {
+            let element = ((items.length - 1) + stepConvert);
 
-        
+            items[element].style.right = stepSlaider * Math.abs(Math.floor(currentPositionslider / stepSlaider).toFixed(3)) + 'px';
         }
+
+        if (steps == 0) {
+            let element = ((items.length - 1) + stepConvert);
+            let stepSlaider = Number(widthStep * (items.length));
+
+            items[element].style.right = stepSlaider.toFixed(3) + 'px';
+        }
+
+        if (steps > 0) {
+            let element = ((items.length - 1) + -stepConvert);
+
+            items[element].style.right = stepSlaider * Math.abs(Math.floor(currentPositionslider / stepSlaider).toFixed(3)) + 'px';
+        }
+            steps = steps - 1;
+        }
+
+
 
     function scroll() {
     document.addEventListener("click",function (e){
         const targetElement = e.target;
         // console.log(targetElement);
-
-            if(targetElement === previosItem){
+            if(targetElement === scrollLeft){
                 nextSlide();
             }
-            if(targetElement === nextItem){
+            if(targetElement === scrollRight){
                 previosSlide()
             }
         });
     };
     scroll();
 }
-spoller()
+
+spoller();
 
 
 
